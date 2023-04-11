5df0262ccc924b1ffaa8fe68e30be3659d545d1b
Maksim Drobyshev <maks88sgt@yandex.ru>
2023-04-11 09:06:03 +0300

    Router

---
+++ armageddon-app/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+import styles from  "./Header.module.css"
+
+export const Header = ()=>{
+    return <div className={styles.container}>
+        <div>
+            <h1>ARMAGGEDON V</h1>
+            <div>Сервис мониторинга и уничтожения астероидов, опасно подлетающих к Земле.</div>
+        </div>
+        <div>
+            <Link to={"/asteroids"}>Астероиды</Link>
+            <Link to={"/destroyment"}>Уничтожение</Link>
+        </div>
+    </div>
+}
