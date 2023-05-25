2f8ea18d9e9173381b5cbe5e84394b71eb9ffe76
Maksim Drobyshev <maks88sgt@yandex.ru>
2023-05-25 16:39:33 +0300

    tests

--- armageddon-app/src/components/header/Header.tsx
+++ armageddon-app/src/components/header/Header.tsx
@@ -3,30 +3,50 @@ import styles from  "./Header.module.css"
 import {getUserKey} from "../../utils/getUserKey";
 import {memo, useState} from "react";
 
-export const Header = memo(()=>{
+export const Header = memo(({someFunc}:{someFunc?: (arg: any)=>void})=>{
     const [inputOpened, setInputOpened] = useState(false)
 
 
-    return <div className={styles.container}>
-        <div>
-            <h1>ARMAGGEDON V</h1>
-            <div>Сервис мониторинга и уничтожения астероидов, опасно подлетающих к Земле.</div>
+    return (
+        <div className={styles.container}>
+            <div>
+                <h1>ARMAGGEDON V</h1>
+                <div>
+                    Сервис мониторинга и уничтожения астероидов, опасно
+                    подлетающих к Земле.
+                </div>
+            </div>
+            <div>
+                <Link to={'/asteroids'}>Астероиды</Link>
+                <Link to={'/destroyment'}>Уничтожение</Link>
+            </div>
+            <div>
+                {getUserKey() === 'DEMO_KEY' ? (
+                    <button
+                        onClick={() => {
+                            someFunc("123")
+                            setInputOpened(!inputOpened);
+                        }}
+                    >
+                        Unauthorized
+                    </button>
+                ) : (
+                    <div>Api key provided</div>
+                )}
+            </div>
+            {inputOpened ? (
+                <input
+                    data-testId={'api_key_input'}
+                    onChange={(ev) => {
+                        if (ev.target.value.length == 40) {
+                            localStorage.setItem('API_KEY', ev.target.value);
+                            setInputOpened(false);
+                        }
+                    }}
+                />
+            ) : null}
         </div>
-        <div>
-            <Link to={"/asteroids"}>Астероиды</Link>
-            <Link to={"/destroyment"}>Уничтожение</Link>
-        </div>
-        <div>
-            {getUserKey() === "DEMO_KEY" ? <button onClick={()=>setInputOpened(!inputOpened)}>Unauthorized</button> : <div>Api key provided</div>}
-        </div>
-        {inputOpened ? <input onChange={(ev)=>{
-            if (ev.target.value.length == 40) {
-                localStorage.setItem("API_KEY",ev.target.value);
-                setInputOpened(false)
-            }
-        }
-        }/> : null}
-    </div>
+    );
 })
 
 Header.displayName = "Header"
---
+++ armageddon-app/src/components/header/Header.unit.test.tsx
@@ -0,0 +1,38 @@
+import { Header } from './Header';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AsteroidsContextProvider } from "../asteroids-context/AsteroidsContext";
+
+describe('[components] Header', () => {
+    const mockFunction = jest.fn()
+    beforeEach(() => {
+        render(
+          <AsteroidsContextProvider>
+            <BrowserRouter>
+                <Header someFunc={mockFunction}/>
+            </BrowserRouter>
+          </AsteroidsContextProvider>
+        );
+    });
+    test('should contains h1 html element', () => {
+        const header = screen.getByRole('heading');
+        expect(header).toBeInTheDocument();
+        expect(header).toHaveTextContent('ARMAGGEDON V');
+    });
+    it('should contains links to asteroids and destroyment pages', () => {
+        const links = screen.getAllByRole('link');
+        expect(links[0]).toHaveAttribute('href', '/asteroids');
+        expect(links[1]).toHaveAttribute('href', '/destroyment');
+    });
+    it('should contains button, after click on it should hide button and display input', () => {
+        const button = screen.getByText('Unauthorized');
+        expect(button).toBeInTheDocument();
+        expect(screen.queryByTestId('api_key_input')).not.toBeInTheDocument();
+        fireEvent.click(button);
+ 1
+        waitFor(() => {
+            expect(button).not.toBeInTheDocument();
+            expect(screen.getByTestId('api_key_input')).toBeInTheDocument();
+        });
+    });
+});
